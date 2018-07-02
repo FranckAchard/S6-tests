1c0bbaaac7d5937d0db58c58cf5e6cc566d3df35
FranckAchard <36164145+FranckAchard@users.noreply.github.com>
2018-07-02 15:03:40 +0200

    real first commit

---
+++ src/main/java/co/simplon/test/Smileys.java
@@ -0,0 +1,10 @@
+package co.simplon.test;
+
+import java.util.List;
+
+public class Smileys {
+
+	public static int countSmileys(List<String> arr) {
+		  return arr != null ? arr.size() : 0;
+		}
+}
---
+++ src/main/java/co/simplon/test/SmileysTest.java
@@ -0,0 +1,31 @@
+package co.simplon.test;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+public class SmileysTest {
+
+	@Test
+	public void shouldReturn0IfNull() {
+		assertEquals(0, Smileys.countSmileys(null));
+	}
+	
+	@Test
+	public void shouldReturn0IfEmpty() {
+		List<String> arr = new ArrayList<String>();
+		assertEquals(0, Smileys.countSmileys(arr));
+	}
+	
+	@Test
+	public void shouldReturn1IfOneSmiley() {
+		List<String> arr = new ArrayList<String>();
+		arr.add("r");
+		assertEquals(1, Smileys.countSmileys(arr));
+	}
+	
+
+}
