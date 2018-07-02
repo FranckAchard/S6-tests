c78bc4188598e31fd7a35716131970488ea66409
FranckAchard <36164145+FranckAchard@users.noreply.github.com>
2018-07-02 16:17:56 +0200

    exo camel case

--- src/main/java/co/simplon/test/CamelCase.java
+++ src/main/java/co/simplon/test/CamelCase.java
@@ -2,6 +2,22 @@ package co.simplon.test;
 
 public class CamelCase {
 	public static String camelCase(String str) {
-		return null;
+		String res = "";
+		if (str != null) {
+			// on decoupe la chaine en tableau de chaines en passant les caracteres separateurs en expression reguliere 
+			for (String wd : str.split("[_-]")) {
+				res += camelCaseOneWord(wd);
+			}
+		}
+		else {
+			res = "null!";
+		}
+
+		return res;
+	}
+	
+	private static String camelCaseOneWord(String word) {
+		// mettre la 1e lettre en majuscule et les suivantes en minuscules
+		return Character.toUpperCase(word.charAt(0)) + word.substring(1).toLowerCase();
 	}
 }
--- src/main/java/co/simplon/test/CamelCaseTest.java
+++ src/main/java/co/simplon/test/CamelCaseTest.java
@@ -5,10 +5,28 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class CamelCaseTest {
+	
+	@Test
+	public void shoudReturnSthIfNull () {
+		assertEquals("null!", CamelCase.camelCase(null));
+	}
 
 	@Test
-	public void shouldReturnStrIfOneWord() {
-		String str = "Toto";
-		assertEquals(str, CamelCase.camelCase(str));
+	public void shouldReturnUpperStrIfOneWord() {
+		String str = "toto";
+		assertEquals("Toto", CamelCase.camelCase(str));
 	}
+	
+	@Test
+	public void shouldReturnCamelOneWord() {
+		String str = "tOTO";
+		assertEquals("Toto", CamelCase.camelCase(str));	
+	}
+	
+	@Test
+	public void shoudlReturnCamelWordsWithUnderOrHyphen() {
+		String str = "toto_tata-tITI";
+		assertEquals("TotoTataTiti", CamelCase.camelCase(str));	
+	}
+	
 }
